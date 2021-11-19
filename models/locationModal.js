71b9c11df56c4da969ee31700730044424a66417
izuru775 <izuru775@gmail.com>
2021-11-20 01:35:22 +1100

    Location modal created

---
+++ models/locationModal.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const locationSchema = new mongoose.Schema({
+    name: { 
+        type: String,
+        required: true 
+    },
+    long: { 
+        type: Number,
+        required: true 
+    },
+    lat: {
+        type: Number,
+        required: true 
+    },
+    billboard: { 
+        type: String, 
+        required: true 
+    }
+});
+
+const location = mongoose.model('Location',locationSchema)
+
+module.exports = location;
