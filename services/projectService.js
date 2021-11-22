d69bf6205f91f87fee0739bf83cecaf11c5d9063
izuru775 <izuru775@gmail.com>
2021-11-22 16:47:06 +1100

    InsertProject service initialized

--- services/projectService.js
+++ services/projectService.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
 const Location = require('../models/locationModal');
+const Controllers = require('../controllers/index');
 
+
+const inserProject =(req,res,obj)=>{
+
+}
+
+exports.inserProject = {
+    
+}
