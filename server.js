f0316d312bfba0674a04ed689c0175024ddd3aa2
izuru775 <izuru775@gmail.com>
2021-11-22 16:45:00 +1100

    api route created

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const { mongo } = require('mongoose');
 const path = require('path');
 const dbConnect = require('./dbConnect');
 
+
 const app = express();
 
 app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended:true}));
+
+app.use('/api',require('./routes/index'));
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'public','404.html'));
