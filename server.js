2f803dc5413da427cfb061ae98ef7f0aa78cd57d
izuru775 <izuru775@gmail.com>
2021-11-24 01:35:08 +1100

    Required routes

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const { mongo } = require('mongoose');
 const path = require('path');
+const apiRoute = require('./routes/projectRoutes')
 const dbConnect = require('./dbConnect');
 
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended:true}));
 
-app.use('/api',require('./routes/index'));
+app.use('/',apiRoute);
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'public','404.html'));
