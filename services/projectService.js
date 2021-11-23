b22698855ac19692ed7f741168d440cf7bf5a724
izuru775 <izuru775@gmail.com>
2021-11-24 01:37:49 +1100

    InsertProject and getAllProjects services added

--- services/projectService.js
+++ services/projectService.js
@@ -3,10 +3,36 @@ const Location = require('../models/locationModal');
 const Controllers = require('../controllers/index');
 
 
-const inserProject =(req,res,obj)=>{
 
+const inserProject = (req, res, obj) => {
+    const { name, lat, lng, billboard } = obj;
+    const newLocation = new Location({
+        name,
+        lat,
+        lng,
+        billboard
+    })
+    newLocation.save()
+        .then(location =>{
+            if(location){
+                res.json({statusCode: 200,message:"Location sucessfully added",data:location})
+            }else{
+                res.json({statusCode:400, message:"error"})
+            }
+        })
+        .catch(err=> console.log(err))
 }
 
-exports.inserProject = {
-    
+const getAllprojects = (req,res)=>{
+    Location.find({})
+        .then(result=>{
+            if(result){
+                res.json({statusCode:200,data:result})
+            }else{
+                res.json({statusCode:400,message:"Error"})
+            }
+        })
+        .catch(error=>console.log(error))
 }
+exports.inserProject = inserProject;
+exports.getAllprojects = getAllprojects;
