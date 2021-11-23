243a02302d3bc2057f43c8df867945b806da25f0
izuru775 <izuru775@gmail.com>
2021-11-24 01:35:50 +1100

    GetProjects controller created

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -2,14 +2,21 @@ const Services = require('../services/index');
 
 const createProject = (req,res)=>{
     const obj ={
-            name: "Location 1",
-            lat: "-37.80625873487332",
-            lng: "144.9592512868676",
-            billboard: "public/images/..."
+            name: "Location 4",
+            lat: "-37.8502000551176",
+            lng: "145.09756638589258",
+            billboard: "https://lh5.googleusercontent.com/p/AF1QipOBm5ug0sZvMVEUXfkdnBI6fYIVsLhMNFTwZqIN=w484-h240-k-no"
+
+            // Location 2(deakin) -37.84614523777127, 145.11489061370446
+            // Location 3(VicRoads Burwood) -37.855430123671944, 145.1650157338754
+            // Location 4(McDonald's Burwood) -37.8502000551176, 145.09756638589258
     }
     Services.projectService.inserProject(req,res,obj)
 }
-
+const getProjects = (req,res) =>{
+    Services.projectService.getAllprojects(req,res);
+}
 module.exports ={
-    createProject
+    createProject,
+    getProjects
 }
